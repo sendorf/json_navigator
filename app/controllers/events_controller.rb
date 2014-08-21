1dcaab99373be2206902b23161ca1bdb0af21c8b
sendorf <sendorf@gmail.com>
2014-08-21 14:50:15 +0200

    Orders the different sports by "pos" attribute

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
 
   def show
-    @sport = Sport.get_sport(List.list["sports"], params[:sport_id])
+    @sport = Sport.get_sport(List.list["sports"].sort_by{|sport| sport["pos"].to_i}, params[:sport_id])
     @event = Event.get_event(@sport["events"],params[:id])
     @outcomes = @event["outcomes"]
   end
